fbae073c210f07f010372c8d6b5941b60aad302b
mufidmb085 <mufidmb085@gmail.com>
2025-01-09 07:52:32 +0700

    Add monospace font

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,6 +1,11 @@
-import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+import { Plus_Jakarta_Sans as FontSans, Inter as FontMono } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
 });
