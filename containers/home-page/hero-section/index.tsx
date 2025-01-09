04e6947b1fffad0510d5d587c44a9201ae232e68
mufidmb085 <mufidmb085@gmail.com>
2025-01-09 08:08:57 +0700

    Update hero section image position to center

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -13,8 +13,8 @@ const HeroSection = () => {
         <div className="embla__viewport" ref={emblaRef}>
           <div className="embla__container">
             {heroSection.map((hero, i) => (
-              <div className="embla__slide" key={i}>
-                <Image className="z-0" src={hero.src} alt={hero.alt} radius="none" style={{ objectFit: "cover", objectPosition: "center" }} />
+              <div className="embla__slide relative flex items-center justify-center" key={i}>
+                <Image removeWrapper={true} className="z-0" src={hero.src} alt={hero.alt} radius="none" style={{ objectFit: "cover", objectPosition: "center" }} />
                 <div className="flex flex-col justify-center items-center absolute z-10 bg-content1/30 inset-0" key={i}>
                   <h1 className="font-black text-primary text-2xl sm:text-4xl uppercase">{hero.title}</h1>
                   <h2 className="font-normal text-default-foreground text-xl sm:text-2xl">{hero.subtitle}</h2>
